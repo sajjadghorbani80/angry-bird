1f7c9ae8ba7a17553afef736ebd98474397e42f7
Hanieh Ghassemi <haniehghsmie@gmail.com>
2023-07-11 03:36:38 +0330

    sound setting

--- Assets/Scripts/levelMenuEvent.cs
+++ Assets/Scripts/levelMenuEvent.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class levelMenuEvent : MonoBehaviour
 {
+
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    private float value;
+    private void Start()
+    {
+        mixer.GetFloat("volume", out value);
+        volumeSlider.value = value;
+    }
+    public void SetVolume()
+    {
+        mixer.SetFloat("volume", volumeSlider.value);
+    }
     public void LoadLevel(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex);
-        
+
     }
 
-    public void GoToLevelMenu(int sceneIndex) {
+    public void GoToLevelMenu(int sceneIndex)
+    {
         PlayerPrefs.SetInt("page", 1);
         SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
-        
+
     }
 
-    public void GoToHomeMenu(int sceneIndex) {
+    public void GoToHomeMenu(int sceneIndex)
+    {
         PlayerPrefs.SetInt("page", 0);
         SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
-        
+
     }
 }
